c6545c47164cd1ff6f32d5d3c455e09de294d624
gccosta13 <gccosta2005@gmail.com>
2025-02-21 13:06:55 +0000

    variaveis

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numero_1 = 9;
+            int numero_2 = 3;
+            int numero_3 = 8;
+
+            Console.WriteLine(numero_1);
+            Console.WriteLine(numero_2);
+            Console.WriteLine(numero_3);
+            
+        }
+    }
+}
