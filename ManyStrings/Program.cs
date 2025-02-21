f1528fb0a5362c6fc62fa2acbae8a81542983a39
gccosta13 <gccosta2005@gmail.com>
2025-02-21 13:50:07 +0000

    string2

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -10,11 +10,20 @@ namespace ManyStrings
             string var_2 = "teste \u225B";
             string var_3 = "teste \n";
             string var_4 = "teste \t";
+            string conca = "teste" + 1;
+            int numero = 1;
+            string inter = $"teste {numero}";
+             
+
 
             Console.WriteLine(var_1);
             Console.WriteLine(var_2);
             Console.WriteLine(var_3);
             Console.WriteLine(var_4);
+            Console.WriteLine(conca);
+            Console.WriteLine(inter);
+            
+
 
         }
     }
