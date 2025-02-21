64d88ec86580c139fdcc940e2ae80fc37d67d51e
gccosta13 <gccosta2005@gmail.com>
2025-02-21 14:24:58 +0000

    exercicio sexta

---
+++ Percentagem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Resolvers;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            float num3 = float.Parse(Console.ReadLine()); 
+            float num4 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1:P2}\n{num2:P2}\n{num3:P2}\n{num4:P2}");
+        }
+
+        }
+    }
+
