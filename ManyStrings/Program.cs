5e389946a5f06bc765a39c44ce45f6ee81943bc1
gccosta13 <gccosta2005@gmail.com>
2025-02-21 13:40:56 +0000

    string

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string var_1 = "teste";
+            string var_2 = "teste \u225B";
+            string var_3 = "teste \n";
+            string var_4 = "teste \t";
+
+            Console.WriteLine(var_1);
+            Console.WriteLine(var_2);
+            Console.WriteLine(var_3);
+            Console.WriteLine(var_4);
+
+        }
+    }
+}
