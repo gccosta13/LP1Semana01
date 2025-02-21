75243e4388bc36bfde11d96bf7a16064e0460c6b
gccosta13 <gccosta2005@gmail.com>
2025-02-21 13:16:09 +0000

    unicode

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -13,6 +13,15 @@ namespace VariousTypes
             Console.WriteLine(numero_1);
             Console.WriteLine(numero_2);
             Console.WriteLine(numero_3);
+
+            char var_1 = '\u2720';
+            char var_2 = '\u2671';
+            char var_3 = '\u2627';
+
+            Console.WriteLine(var_1);
+            Console.WriteLine(var_2);
+            Console.WriteLine(var_3);
+            
             
         }
     }
